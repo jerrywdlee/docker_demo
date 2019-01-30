2b9b5654d178c1b81777bf5cd71f822f7b18243c
Jerry Lee <lee@idoctor.co.jp>
2019-01-30 14:22:22 +0900

    add redis

--- docker-compose/node-demo-app/app.js
+++ docker-compose/node-demo-app/app.js
@@ -25,6 +25,27 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
+const Redis = require('ioredis');
+const REDIS_HOST = process.env['REDIS_HOST'] || '127.0.0.1';
+const REDIS_PORT = process.env['REDIS_PORT'] ||6379;
+const redis = new Redis({ host: REDIS_HOST, port: REDIS_PORT });
+const psub = new Redis({ host: REDIS_HOST, port: REDIS_PORT });
+// redis.set('hoge', 'fuga', 'EX', 10);
+redis.config('set', 'notify-keyspace-events', 'Ex');
+// const psubKey = '__key*@*__:*'; // All key events & spaces in all DB
+const psubKey = '__keyevent@0__:expired'; // All key events & spaces in all DB
+psub.psubscribe(psubKey, (err, count) => {
+  if (err) {
+    console.error('err', err);
+  }
+  // console.log('count', count);
+});
+psub.on('pmessage', (pattern, channel, message) => {
+  console.log('pattern', pattern);
+  console.log('channel', channel);
+  console.log('message', message);
+});
+
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
 
@@ -34,7 +55,11 @@ io.on('connection', (socket) => {
     .on('io-ping', (msg) => {
       console.log(`io-ping: ${msg}`);
       socket.emit('io-pong', msg);
+      redis.set(`io-ping@${msg}`, msg, 'EX', 10);
     })
+  psub.on('pmessage', (pattern, channel, message) => {
+    socket.emit('key-expired', message);
+  });
 })
 
 server.listen(PORT);
